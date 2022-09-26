6b2531b1e2f40624b6637971926fdfcf62ac22de
Hanh20021706 <hanhs8155@gmail.com>
2022-09-26 16:46:34 +0700

    refactor : update demo storybook

--- src/components/result/result.stories.tsx
+++ src/components/result/result.stories.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import { Result } from './result';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Button',
-  component: Button,
+  title: 'Result',
+  component: Result,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof Result>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Result> = (args) => <Result {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
@@ -23,18 +23,8 @@ Primary.args = {
   label: 'Kiểm tra',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Kiểm tra',
-};
-
 export const wrong = Template.bind({});
 wrong.args = {
-    size: 'small',
-    label: 'Kiểm tra'
-};
-export const success = Template.bind({});
-success.args = {
-   
-    label: 'Kiểm tra'
+  size: 'small',
+    label: 'Tiếp tục'
 };
